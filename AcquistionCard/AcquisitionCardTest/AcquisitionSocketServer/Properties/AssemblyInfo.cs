9733706fc7b27105cf370cc55699d6718b8c759f
yandi <1197854157@qq.com>
2019-09-17 07:13:41 +0800

    提交

--- AcquistionCard/AcquisitionCardTest/AcquisitionSocketServer/Properties/AssemblyInfo.cs
+++ AcquistionCard/AcquisitionCardTest/AcquisitionSocketServer/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AcquisitionSocketServer")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2019 DMY. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
